7b3bc65252ac280ab3d75361db19aac59b1e1525
Jason & Tal <Jasonandtal@gmail.com>
2016-04-04 14:38:34 -0700

    add sorting functionality

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -4,8 +4,8 @@ export default Ember.Component.extend({
   fullLocation: Ember.computed('city.name', 'city.country', function() {
     return this.get('city.name') + ', ' + this.get('city.country');
   }),
-  sortBy: ['name:asc'],
-  sortedRentals: Ember.computed.sort('city.name', 'sortBy'),
+  sortBy: ['cost:asc'],
+  sortedListings: Ember.computed.sort('city.listings', 'sortBy'),
   actions: {
   deleteCity(city) {
       if(confirm('Are you sure you want to delete this rental?')) {
